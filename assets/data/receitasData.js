35269df3ee8e3c95a88291ec79b6c68c6a2bd62b
migasrcf <miguellgomes64@gmail.com>
2021-10-14 09:49:24 -0300

    receitasWrapperUpdate

--- assets/data/receitasData.js
+++ assets/data/receitasData.js
@@ -1,28 +1,28 @@
 const receitasData = [
-    {
-        id: "receita-1",
-        title: "Cardiologista",
-        nomeMedico: "Dr. Miguel",
-        date: "07/10/2021",
-        image: require('../images/cardio-1.png'),
-        imageBig: require('../images/big-image-cardio.png'),
-        remedioUm: "Clonazepam",
-        descUm: "2mg, 30 Comprimidos.",
-        remedioDois: "Azitromicina",
+  {
+    id: 'receita-1',
+    title: 'Cardiologista',
+    nomeMedico: 'Dr. Miguel',
+    date: '07/10/2021',
+    image: require('../images/cardio-1.png'),
+    imageBig: require('../images/big-image-cardio.png'),
+    remedioUm: 'Clonazepam',
+    descUm: '2mg, 30 Comprimidos.',
+    /*remedioDois: "Azitromicina",
         descDois: "500mg, 5 Comprimidos.",
         remedioTres: "Expec Xarope",
-        DescTres: "120 ml",
-    },
-    {
-        id: "receita-2",
-        title: "Oftalmologista",
-        nomeMedico: "Dr. Samuel",
-        date: "04/10/2021",
-        image: require('../images/oftalmo-1.png'),
-        imageBig: require('../images/oftalmo-1.png'),
-        remedioUm: "Colírio Lacrifilm",
-        descUm: "10 ml",
-    }
-]
+        DescTres: "120 ml",*/
+  },
+  {
+    id: 'receita-2',
+    title: 'Oftalmologista',
+    nomeMedico: 'Dr. Samuel',
+    date: '04/10/2021',
+    image: require('../images/oftalmo-1.png'),
+    imageBig: require('../images/oftalmo-1.png'),
+    remedioUm: 'Colírio Lacrifilm',
+    descUm: '10 ml',
+  },
+];
 
 export default receitasData;
--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,116 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import colors from '../assets/colors/colors';
+import Feather from 'react-native-vector-icons/Feather';
+import farmaciasData from '../assets/data/farmaciasData';
+import receitasCategoriesData from '../assets/data/receitasCategoriesData';
+import receitasData from '../assets/data/receitasData';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import icone from '../assets/images/Icone.png';
+
+Feather.loadFont();
 
 const Home = ({navigation}) => {
+  const renderReceitaItem = ({item}) => {
+    return (
+      <TouchableOpacity>
+        <ImageBackground
+          source={item.Image}
+          style={styles.receitaItem}
+          imageStyle={styles.receitaItemImage}>
+          <Text style={styles.receitaItemTitle}>{item.title}</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button onPress={() => navigation.navigate('Medico')} title="Click" />
+    <View style={styles.container}>
+      <ScrollView>
+        {/*Header*/}
+        <SafeAreaView>
+          <View style={styles.menuWrapper}>
+            <Feather
+              name="menu"
+              size={32}
+              color={colors.black}
+              style={styles.menuIcon}
+            />
+            <Image source={icone} style={styles.profileImage} />
+          </View>
+        </SafeAreaView>
+
+        {/*Receitas*/}
+        <View style={styles.wrapperReceitas}>
+          <Text style={styles.titleReceitas}>Receitas</Text>
+          <View style={styles.wrapperCategoriesReceitas}>
+            <Text
+              style={[styles.categoriesReceitasText, {color: colors.green}]}>
+              Geral
+            </Text>
+            <Text style={styles.categoriesReceitasText}>Cardiologista</Text>
+            <Text style={styles.categoriesReceitasText}>Oftalmologista</Text>
+            <Text style={styles.categoriesReceitasText}>Neurologista</Text>
+          </View>
+          <View style={styles.wrapperItemsReceitas}>
+            <FlatList
+              data={receitasData}
+              renderItem={renderReceitaItem}
+              keyExtractor={item => item.id}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: colors.white,
+  },
+  menuWrapper: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 52,
+    height: 52,
+  },
+  wrapperReceitas: {
+    marginHorizontal: 20,
+    marginTop: 27,
+  },
+  titleReceitas: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 32,
+    color: colors.black,
+  },
+  wrapperCategoriesReceitas: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  categoriesReceitasText: {
+    marginRight: 30,
+    fontFamily: 'Lato-Regular',
+    fontSize: 16,
+    color: colors.gray,
+  },
+});
 
 export default Home;
