f3ac6b82dfb51856663269f75d96d161e7fe40e7
migasrcf <miguellgomes64@gmail.com>
2021-10-15 10:26:22 -0300

    remedioScreenLayout

--- components/Medico.js
+++ components/Medico.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
+import colors from '../assets/colors/colors';
 
-const Medico = () => {
+const Medico = ({route, navigation}) => {
+  const {item} = route.params;
   return (
-    <View>
-      <Text>Medico</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={item.imageBig}
+        style={styles.backgroundImage}></ImageBackground>
+      <View style={styles.remediosWrapper}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  backgroundImage: {
+    width: 100,
+    height: 100,
+  },
+  remediosWrapper: {},
+});
 
 export default Medico;
